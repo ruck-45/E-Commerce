f7b4b3800101ec63bf39ecedf518edb869e36af1
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-03-13 16:59:38 +0530

    explore product Home page responsive

--- src/components/Home/SubComponents/Explore.tsx
+++ src/components/Home/SubComponents/Explore.tsx
@@ -3,59 +3,52 @@ import React from "react";
 
 const Explore = () => {
   return (
-    <div className="bg-white h-auto">
-      <div className="flex flex-row items-center justify-between md:p-[3rem] p-[1rem]">
-        {/* content */}
-        <div className="flex flex-col items-start justify-between px-[2rem]">
-          <h1 className="text-4xl font-bold py-[1rem]">Explore Our Products</h1>
-          <p className="text-gray-600 py-[0.5rem]">
-            You are allowed to use this HexaShop HTML CSS template. You can feel free to modify or edit this layout. You
-            can convert this template as any kind of ecommerce CMS theme as you wish.
-          </p>
-          <p className="text-gray-600 py-[0.5rem]">
-            There are 5 pages included in this HexaShop Template and we are providing it to you for absolutely free of
-            charge at our TemplateMo website. There are web development costs for us.
-          </p>
-          <p className="text-gray-600 py-[0.5rem]">
-            If this template is beneficial for your website or business, please kindly support us a little via PayPal.
-            Please also tell your friends about our great website. Thank you.
-          </p>
+    <div className="flex flex-col xl:flex-row items-center justify-between md:p-[3rem] p-[1rem]">
+      {/* content */}
+      <div className="flex flex-col items-start justify-between px-[2rem] max-w-[40rem]">
+        <h1 className="text-4xl font-bold py-[1rem]">Explore Our Products</h1>
+        <p className="text-gray-600 py-[0.5rem] text-justify">
+          Step into a world of refined sophistication with our curated selection of carpets, rugs, and decorative
+          antiques. At our store, we offer a diverse range of exquisite pieces that redefine luxury living.
+        </p>
+        <p className="text-gray-600 py-[0.5rem] text-justify">
+          From intricately woven rugs to ornate decorative antiques, each item tells a story of craftsmanship and
+          elegance. Explore our collection and immerse yourself in a realm of timeless beauty and charm.
+        </p>
+        <p className="text-gray-600 py-[0.5rem] text-justify">
+          Whether you're looking to add warmth to your living space with a plush carpet or seeking a statement piece to
+          elevate your decor, we have something for every style and taste. Uncover new trends and elevate your home with
+          our exclusive offerings.
+        </p>
 
-          <div className="py-[1rem]">
-            <Button
-            radius="none"
-            className="px-[2.5rem]  text-1xl font-semibold py-[2rem] ">Explore</Button>
-          </div>
+        <div className="py-[1rem]">
+          <Button radius="none" className="px-[2.5rem]  text-1xl font-semibold py-[2rem] ">
+            Explore
+          </Button>
         </div>
+      </div>
 
-        {/* image */}
-        <div className="md:flex flex-col justify-center items-center gap-4 p-[4rem] lg:inline-block hidden">
-          <div className="flex flex-row items-center justify-center gap-2 p-[1rem]">
-            <div className="bg-slate-100 w-[15rem] h-[15rem] flex flex-col justify-center items-center ">
-              <h1 className="text-2xl font-bold text-center">Carpets</h1>
-              <p className="text-center opacity-30">Latest Collection</p>
-            </div>
-            <div className=" w-[18rem] h-[15rem] p-[1rem] flex justify-center items-center">
-              <img
-                className="w-[15rem] h-[15rem]"
-                src="https://img.freepik.com/free-photo/multiple-carpets-grand-bazaar-istanbul-turkey_1268-21687.jpg?w=360&t=st=1710156440~exp=1710157040~hmac=29cc1d9a61bd3646a6a69847ae86630b0ed71d75d7eafa685040b93aef087f58"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-center gap-2 p-[1rem]">
-            <div className=" w-[18rem] h-[15rem] p-[1rem] flex justify-center items-center">
-              <img
-                className="w-[15rem] h-[15rem]"
-                src="https://img.freepik.com/premium-photo/filigree-bottle-full-amber-liquid-photorealistic_777271-4904.jpg?w=360"
-                alt=""
-              />
-            </div>
-            <div className="bg-slate-100 w-[18rem] h-[15rem] flex flex-col justify-center items-center ">
-              <h1 className="text-2xl font-bold text-center">Different Types</h1>
-              <p className="text-center opacity-30">Over 304 Products</p>
-            </div>
-          </div>
+      {/* image */}
+      <div className="grid-cols-2 gap-[1rem] shrink-0 hidden md:grid">
+        <div className="bg-slate-100 flex flex-col justify-center items-center hover:scale-105 transition-all cursor-pointer">
+          <h1 className="text-2xl font-bold text-center">Carpets</h1>
+          <p className="text-center opacity-30">Latest Collection</p>
+        </div>
+        <div
+          className=" w-[18rem] h-[15rem] bg-cover bg-center bg-norepeat hover:scale-105 transition-all cursor-pointer"
+          style={{
+            backgroundImage: `url(https://images.unsplash.com/photo-1546550879-3b71f2427ae0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+          }}
+        ></div>
+        <div
+          className=" w-[18rem] h-[15rem] bg-cover bg-center bg-norepeat hover:scale-105 transition-all cursor-pointer"
+          style={{
+            backgroundImage: `url(https://images.unsplash.com/photo-1575886672692-34cb9b65a074?q=80&w=1791&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+          }}
+        ></div>
+        <div className="bg-slate-100 flex flex-col justify-center items-center hover:scale-105 transition-all cursor-pointer">
+          <h1 className="text-2xl font-bold text-center">Antiques</h1>
+          <p className="text-center opacity-30">Over 304 Products</p>
         </div>
       </div>
     </div>

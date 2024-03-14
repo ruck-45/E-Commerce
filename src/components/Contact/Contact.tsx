87238107f2b240efc76dfae554e401dc2cd9062b
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-03-14 13:59:09 +0530

    contact page complete

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,6 +5,8 @@ import { useLocation } from "react-router-dom";
 // Local Files
 import EmailForm from "./suComponents/EmailForm";
 import Map from "./suComponents/Map";
+import EmailContact from "../Home/SubComponents/EmailContact";
+import ImageRow from "../Home/SubComponents/ImageRow";
 
 import { scrollTop } from "../../utils/controllers";
 import { updateTab } from "../../Redux/Slices/curTabSlice";
@@ -18,9 +20,13 @@ const Contact = () => {
   scrollTop(offset);
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 justify-start">
-      <EmailForm />
-      <Map />
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 justify-start p-[3rem] md:p-[5rem] gap-[3rem] md:gap-[5rem]">
+        <EmailForm />
+        <Map />
+      </div>
+      <EmailContact />
+      <ImageRow />
     </div>
   );
 };

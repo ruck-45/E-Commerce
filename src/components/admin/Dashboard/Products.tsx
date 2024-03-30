5d3259f11abc2973f595bbd9ecfbbd72e8f53570
AnishMishra1 <anishmishra143@gmail.com>
2024-03-30 17:18:45 +0530

    adding route to edit product

--- src/components/admin/Dashboard/Products.tsx
+++ src/components/admin/Dashboard/Products.tsx
@@ -7,6 +7,7 @@ import { Navigate, useNavigate } from "react-router";
 import { getCookie } from "../../../utils/cookies";
 import { RootState } from "../../../Redux/store";
 import Model from "./Model";
+import { Button } from "@nextui-org/react";
 
 
 
@@ -35,24 +36,11 @@ const Products = () => {
    }
   }
   
+  const EditProduct = (id:string)=>{
+    navigate(`/admin/editProduct/${id}`)
+  }
  
-  // async function deleteItemById(id: string) {
-  //   try {
-  //      console.log("id is", id)
-  //     const response = await axios.delete(`${apiUrl}/items/deleteItem`, {
-  //       data: { id },
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  //     console.log("delete by id", response);
-  //     if (response?.data?.success === true) {
-  //       navigate("/Layout");
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+
  const handleDeleteButtonClick = () => {
    setShowModel(true);
    // Additional logic for handling the delete action, if needed
@@ -76,7 +64,7 @@ const Products = () => {
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
             {/* Sample item */}
             {data.map((item: any) => (
-              <div key={item.item_id} className="group border-2">
+              <div key={item.item_id} className="group border-2 rounded-xl">
                 <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200">
                   <img
                     src={`${apiUrl}/items/itemImages/${item.item_id}_img${activeImage}.jpg`}
@@ -90,10 +78,12 @@ const Products = () => {
                   </div>
                   <p className="text-sm text-gray-700">Rs{item.price}</p>
                   <div className="flex flex-row items-start justify-between py-[1rem]">
-                    <button className="bg-green-600 text-white px-[1rem] rounded-xl">Edit</button>
-                    <button onClick={handleDeleteButtonClick} className="bg-red-600 text-white px-[1rem] rounded-xl">
+                    <Button
+                    onClick={()=> EditProduct(item.item_id)}
+                    className="bg-green-600 text-white px-[1rem] rounded-xl">Edit</Button>
+                    <Button onClick={handleDeleteButtonClick} className="bg-red-600 text-white px-[1rem] rounded-xl">
                       Delete
-                    </button>
+                    </Button>
                   </div>
                 </div>
                 

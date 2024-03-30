92eac4bab7787012968c837079de1c3da77921d3
AnishMishra1 <anishmishra143@gmail.com>
2024-03-30 17:09:43 +0530

    add pop model for delete product

---
+++ src/components/admin/Dashboard/Model.tsx
@@ -0,0 +1,82 @@
+import axios from "axios";
+import { useRef } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { getCookie } from "../../../utils/cookies";
+import { Button } from "@nextui-org/react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../Redux/store";
+
+type CardProps = {
+  onClose: any;
+  jobId: string;
+};
+
+const Model = (props: CardProps) => {
+  const token = getCookie("token");
+  const apiUrl = useSelector((state: RootState) => state.apiConfig.value);
+
+  const navigate = useNavigate();
+  const modalRef = useRef<HTMLDivElement | null>(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const closeModal = (e: any) => {
+    if (modalRef.current === e.target) {
+      props.onClose();
+    }
+  };
+
+  async function deleteItemById() {
+    try {
+      console.log("id is", props.jobId);
+      const response = await axios.delete(`${apiUrl}/items/deleteItem`, {
+        data: { id: props.jobId },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("delete by id", response);
+      if (response?.data?.success === true) {
+        navigate("/Layout");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div
+      ref={modalRef}
+      onClick={closeModal}
+      className="fixed inset-0 z-50 backdrop-blur-md flex justify-center items-center"
+    >
+      <div className="flex flex-col w-[30rem] h-auto bg-slate-600 items-center justify-center ">
+        <div className="p-[3rem]">
+          <p className="text-white font-semibold text-2xl"> Are You Sure Want to Delete Product?</p>
+        </div>
+
+        <div className="flex justify-center items-center gap-[1rem] p-[2rem]">
+          <Button
+            onClick={props.onClose}
+            className="py-[0.6rem] px-[1rem] bg-green-600 text-white rounded-xl hover:text-yellow-600 hover:bg-white"
+            isLoading={loading}
+          >
+            Close
+          </Button>
+          <Button
+            onClick={deleteItemById}
+            className="py-[0.6rem] px-[1rem] bg-red-600 text-white rounded-xl hover:text-[#F31260] hover:bg-white"
+            isLoading={loading}
+          >
+            Delete
+          </Button>
+        </div>
+      </div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default Model;
--- src/components/admin/Dashboard/Products.tsx
+++ src/components/admin/Dashboard/Products.tsx
@@ -1,11 +1,14 @@
 import axios from "axios";
 import Pagination from "./Pagination";
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { HiOutlineChevronDown } from "react-icons/hi";
 import { useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router";
 import { getCookie } from "../../../utils/cookies";
 import { RootState } from "../../../Redux/store";
+import Model from "./Model";
+
+
 
 
 const Products = () => {
@@ -16,6 +19,7 @@ const Products = () => {
   const apiUrl = useSelector((state: RootState) => state.apiConfig.value);
   const [data, setData] = useState([])
   const [activeImage, setActiveImage] = useState("1"); 
+  const [showModel, setShowModel] = useState(false);
   async function getAllProucts (){
 
    try {
@@ -32,28 +36,32 @@ const Products = () => {
   }
   
  
-  async function deleteItemById(id: string) {
-    try {
-       console.log("id is", id)
-      const response = await axios.delete(`${apiUrl}/items/deleteItem`, {
-        data: { id },
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      console.log("delete by id", response);
-      if (response?.data?.success === true) {
-        navigate("/Layout");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
+  // async function deleteItemById(id: string) {
+  //   try {
+  //      console.log("id is", id)
+  //     const response = await axios.delete(`${apiUrl}/items/deleteItem`, {
+  //       data: { id },
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     console.log("delete by id", response);
+  //     if (response?.data?.success === true) {
+  //       navigate("/Layout");
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+ const handleDeleteButtonClick = () => {
+   setShowModel(true);
+   // Additional logic for handling the delete action, if needed
+ };
 
 
   useLayoutEffect(()=>{
    getAllProucts()
+   console.log("here is model",showModel)
   },[])
 
   return (
@@ -67,29 +75,34 @@ const Products = () => {
 
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
             {/* Sample item */}
-            {data.map((data: any) => (
-              <a href="#" className="group border-2">
+            {data.map((item: any) => (
+              <div key={item.item_id} className="group border-2">
                 <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200">
                   <img
-                    src={`${apiUrl}/items/itemImages/${data.item_id}_img${activeImage}.jpg`}
+                    src={`${apiUrl}/items/itemImages/${item.item_id}_img${activeImage}.jpg`}
                     alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
                     className="object-cover object-center group-hover:opacity-75"
                   />
                 </div>
                 <div className="px-[1rem]">
                   <div className="w-[14rem]">
-                    <h3 className="text-1xl font-semibold mb-2  text-start">{data.title}</h3>
+                    <h3 className="text-1xl font-semibold mb-2  text-start">{item.title}</h3>
                   </div>
-                  <p className="text-sm text-gray-700">Rs{data.price}</p>
-                  <div className="flex flex-row items-start justify-between  py-[1rem]">
+                  <p className="text-sm text-gray-700">Rs{item.price}</p>
+                  <div className="flex flex-row items-start justify-between py-[1rem]">
                     <button className="bg-green-600 text-white px-[1rem] rounded-xl">Edit</button>
-                    <button onClick={()=>deleteItemById(data.item_id)} className="bg-red-600 text-white px-[1rem] rounded-xl">Delete</button>
+                    <button onClick={handleDeleteButtonClick} className="bg-red-600 text-white px-[1rem] rounded-xl">
+                      Delete
+                    </button>
                   </div>
                 </div>
-              </a>
+                
+                {showModel && <Model onClose={() => setShowModel(false)} jobId={item.item_id}/>}
+              </div>
             ))}
           </div>
           <Pagination />
+        
         </div>
       </div>
     </>

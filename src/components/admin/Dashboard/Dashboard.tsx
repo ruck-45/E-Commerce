23495a08d258aa6141011b17a8c3c5885b4f3b45
AnishMishra1 <anishmishra143@gmail.com>
2024-04-03 13:29:32 +0530

    change ui admin

--- src/components/admin/Dashboard/Dashboard.tsx
+++ src/components/admin/Dashboard/Dashboard.tsx
@@ -1,29 +1,15 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import DashboardStatsGrid from "./DashboardStatsGrid";
-// import TransactionChart from "./TransactionChart";
 import RecentOrders from "./RecentOrders";
-// import PopularProducts from "./PopularProducts";
-// import BuyerProfilePieChart from "./BuyerProfilePieChart";
 import Pagination from "./Pagination";
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-2 ">
       <DashboardStatsGrid />
-      {/* <div className="flex flex-row gap-4 w-full">
-        {/* <TransactionChart /> */}
-      {/* <BuyerProfilePieChart /> */}
-      {/* </div> */}
-      <div className="flex flex-row gap-4 w-full">
-        <RecentOrders />
-
-        {/* <PopularProducts /> */}
-      </div>
-      <div>
-        {" "}
+      <RecentOrders />
         <Pagination />
-      </div>
+      
     </div>
   );
 };

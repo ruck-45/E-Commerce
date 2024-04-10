0c05fcd1e39744df94b8d2bd4c32b2ddae9ea5ea
anil joshi <aniljoshiskip@gmail.com>
2024-04-10 13:46:20 +0530

    api config slice changes commit

--- src/Redux/Slices/apiConfigSlice.ts
+++ src/Redux/Slices/apiConfigSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: process.env.REACT_APP_API_URL,
+  value: process.env.NODE_ENV === "development" ? process.env.REACT_APP_DEV_API_URL : process.env.REACT_APP_API_URL,
 };
 
 const apiConfigSlice = createSlice({
